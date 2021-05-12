0d5b83598074ca31abbc5cb8fe2ebda730c9f27f
sa6o <sa6o.hristov96@gmail.com>
2021-05-12 02:18:24 +0200

    Changed folder names and created walkable page

--- app.py
+++ app.py
@@ -6,9 +6,63 @@ app = Flask(__name__)
 
 #db = SQLAlchemy(app)
 
+language = "en"
+
+# "Chose language" and "Loggin" page
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+# Landing Page for each language
+@app.route('/<string:lang>/home')
+def home(lang):
+    return render_template(lang + '/home.html', language = lang)
+
+# The GEZ Form Part 1 (Generall information)
+@app.route('/<string:lang>/gez/general_information', methods=['GET','POST'])
+def general_information(lang):
+    if request.method == 'POST':
+        ########## Put data from forms into DB ##########
+        
+        #################################################
+        return redirect('/' + lang + '/gez/adress' )
+    else:
+        return render_template(lang + '/general_information.html', language = lang)
+
+# The GEZ Form Part 2 (Adress)
+@app.route('/<string:lang>/gez/adress', methods=['GET','POST'])
+def adress(lang):
+    if request.method == 'POST':
+        ########## Put data from forms into DB ##########
+        
+        #################################################
+        return redirect('/' + lang + '/gez/payment_methods' )
+    else:
+        return render_template(lang + '/adress.html', language = lang)
+
+# The GEZ Form Part 4 (Payment Methods)
+@app.route('/<string:lang>/gez/payment_methods', methods=['GET','POST']) 
+def payment_methods(lang):
+    if request.method == 'POST':
+        ########## Put data from forms into DB ##########
+        
+        #################################################
+        return redirect('/' + lang + '/gez/sepa' )
+    else:
+        return render_template(lang + '/payment_methods.html', language = lang)
+
+# The GEZ Form Part 5 (Sepa Data)
+@app.route('/<string:lang>/gez/sepa', methods=['GET','POST'])
+def sepa(lang):
+    if request.method == 'POST':
+        ########## Put data from forms into DB ##########
+        
+        #################################################
+        return redirect('/' + lang + '/gez/complete' )
+    else:
+        return render_template(lang + '/sepa.html', language = lang)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
