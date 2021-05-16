30ade407365e09383baaba38c35726fac4e87747
sa6o <sa6o.hristov96@gmail.com>
2021-05-16 19:03:28 +0200

    Language chagable

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 #db = SQLAlchemy(app)
 
-language = "en"
+language_glob = "en"
 
 # "Chose language" and "Loggin" page
 @app.route('/')
@@ -15,75 +15,106 @@ def index():
 
 @app.route('/register', methods=['GET','POST'])
 def registration():
+    global language_glob
     if request.method == 'POST':
         ########## Put data from forms into DB ##########
-        language = request.form['language']
+        language_glob = request.form['language']
         #################################################
-        return redirect('/' + language + '/home' )
+        return redirect('/' + language_glob + '/home')
     else:
         return render_template('register.html')
 
 # Landing Page for each language
 @app.route('/<string:lang>/home')
 def home(lang):
-    return render_template(lang + '/home.html', language = lang)
+    global language_glob
+    if request.args.get('lang') != None:
+        return redirect ('/' + request.args.get('lang') + '/home')
+    language_glob = lang
+    print (language_glob)
+    return render_template(language_glob + '/home.html', language = language_glob)
 
 # The GEZ Form Part 1 (Generall information)
 @app.route('/<string:lang>/gez/general_information', methods=['GET','POST'])
 def general_information(lang):
+    global language_glob
     if request.method == 'POST':
         ########## Put data from forms into DB ##########
         
         #################################################
-        return redirect('/' + lang + '/gez/adress' )
+        return redirect('/' + language_glob + '/gez/adress' )
     else:
-        return render_template(lang + '/general_information.html', language = lang)
+        if request.args.get('lang') != None:
+            return redirect ('/' + request.args.get('lang') + '/gez/general_information')
+        language_glob = lang
+        print (language_glob)
+        return render_template(language_glob + '/general_information.html', language = lang)
 
 # The GEZ Form Part 2 (Adress)
 @app.route('/<string:lang>/gez/adress', methods=['GET','POST'])
 def adress(lang):
+    global language_glob
     if request.method == 'POST':
         ########## Put data from forms into DB ##########
         
         #################################################
-        return redirect('/' + lang + '/gez/payment_methods' )
+        return redirect('/' + language_glob + '/gez/payment_methods' )
     else:
-        return render_template(lang + '/adress.html', language = lang)
+        if request.args.get('lang') != None:
+            return redirect ('/' + request.args.get('lang') + '/gez/adress')
+        language_glob = lang
+        return render_template(language_glob + '/adress.html', language = language_glob)
 
 # The GEZ Form Part 4 (Payment Methods)
 @app.route('/<string:lang>/gez/payment_methods', methods=['GET','POST']) 
 def payment_methods(lang):
+    global language_glob
     if request.method == 'POST':
         ########## Put data from forms into DB ##########
         
         
         #################################################
         if request.form["type"] == "sepa":
-            return redirect('/' + lang + '/gez/sepa' )
+            return redirect('/' + language_glob + '/gez/sepa' )
         elif request.form["type"] == "bank":
-            return redirect('/' + lang + '/gez/final')
+            return redirect('/' + language_glob + '/gez/final')
     else:
-        return render_template(lang + '/payment_methods.html', language = lang)
+        if request.args.get('lang') != None:
+            return redirect ('/' + request.args.get('lang') + '/gez/payment_methods')
+        language_glob = lang
+        return render_template(language_glob + '/payment_methods.html', language = language_glob)
 
 # The GEZ Form Part 5 (Sepa Data)
 @app.route('/<string:lang>/gez/sepa', methods=['GET','POST'])
 def sepa(lang):
+    global language_glob
     if request.method == 'POST':
         ########## Put data from forms into DB ##########
         
         #################################################
-        return redirect('/' + lang + '/gez/final' )
+        return redirect('/' + language_glob + '/gez/final' )
     else:
-        return render_template(lang + '/sepa.html', language = lang)
+        if request.args.get('lang') != None:
+            return redirect ('/' + request.args.get('lang') + '/gez/sepa')
+        language_glob = lang
+        return render_template(language_glob + '/sepa.html', language = language_glob)
 
 @app.route('/<string:lang>/gez/final')
 def gez_final(lang):
-    return render_template(lang + '/final.html')
+    global language_glob
+    if request.args.get('lang') != None:
+        return redirect ('/' + request.args.get('lang') + '/gez/final')
+    language_glob = lang
+    return render_template(language_glob + '/final.html')
 
 
 @app.route('/<string:lang>/about')
 def about(lang):
-    return render_template(lang + '/about_us.html', language = lang)
+    global language_glob
+    if request.args.get('lang') != None:
+        return redirect ('/' + request.args.get('lang') + '/about')
+    language_glob = lang
+    return render_template(language_glob + '/about_us.html', language = language_glob)
 
 if __name__ == "__main__":
     app.run(debug=True)
