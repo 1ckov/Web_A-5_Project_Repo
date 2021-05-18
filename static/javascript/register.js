6219e209071e19e64bf3bfe6ff3564b05d5462e6
sa6o <sa6o.hristov96@gmail.com>
2021-05-19 00:49:27 +0200

    registration done

--- static/javascript/register.js
+++ static/javascript/register.js
@@ -1,8 +1,25 @@
 function checkPasswordValidity() {
     var password1 = document.getElementById('password1');
     var password2 = document.getElementById('password2');
+    var url = window.location.href;
+    var res = url.slice(-11,-9);
+
     if (password1.value != password2.value) {
-        password2.setCustomValidity('Passwords has to be the same!');
+        if (res === "en"){
+            password2.setCustomValidity('Passwords have to be the same!');
+        }
+        else if( res === "de"){
+            password2.setCustomValidity('Passwörter müssen übereinstimmen!');
+        }
+        else if( res === "bg"){
+            password2.setCustomValidity('Паролите трябва да са еднакви!');
+        }
+        else if( res === "fr"){
+            password2.setCustomValidity('Les mots de passe doivent être les mêmes!');
+        }
+        else {
+            password2.setCustomValidity('Passwords has to be the same!');
+        }
     } else {
         password2.setCustomValidity('');
     }
