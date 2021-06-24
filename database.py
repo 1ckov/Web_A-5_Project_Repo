f84c06d9bfbd9dc4d8a29d07dca662d90c6813c5
Muhammed-git <1926769@stud.hs-mannheim.de>
2021-06-24 15:57:06 +0200

    Datenbank für 3b, Beziehung in Kommentaren

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ from sqlalchemy import Column, Date, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.sql.visitors import _generate_dispatcher
-from sqlalchemy_utils import EmailType
+from sqlalchemy_utils import EmailType, PhoneNumber
 
 
 
@@ -24,6 +24,29 @@ class User(Base):
     first_name = Column(String,nullable=true)
     last_name = Column(String,nullable=true)
     date_of_birth = Column(Date,nullable=true)
+    ##daten = relationship('daten', backref='benutzer', uselist=false)
 
+
+class Daten(Base):
+
+    __tablename__ = "daten"
+    id = Column(Integer ,primary_key=true,)
+     ## User_id = Column(Integer, Foreignkey('User_id'),unique=true)                                                   
+    gender = Column(String,nullable=true)
+    first_name = Column(String,nullable=true)
+    last_name = Column(String,nullable=true)
+    date_of_birth = Column(Date,nullable=true)
+    street = Column(String,nullable=true)
+    streetnumber = Column(String,nullable=true)
+    address_addition = Column(String,nullable=true)
+    zip_code = Column(Integer,nullable=true)
+    city = Column(String,nullable=true)
+    phone_number = Column(PhoneNumber,nullable=true)
+    timespan = Column(String,nullable=true)
+    type_of_transfer = Column(Boolean,nullable=true)            ##true = sepa## 
+    name = Column(String,nullable=true)
+    IBAN = Column(String,nullable=true)
+    BIC = Column(String,nullable=true)
+    credit_institution = Column(String,nullable=true)
 
 Base.metadata.create_all(engine)
